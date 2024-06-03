ee21e4437f5bea45e1c789dc92c3eadf5850f4b8
Muhammed Ergül <futurama.99@hotmail.de>
2024-06-03 23:06:03 +0200

    refactor: improvement of mongodb

--- persistence/src/main/scala/de/htwg/se/mill/databaseComponent/MongoDB/MongoDBDAO.scala
+++ persistence/src/main/scala/de/htwg/se/mill/databaseComponent/MongoDB/MongoDBDAO.scala
@@ -1,99 +1,73 @@
 package databaseComponent.MongoDB
-import org.mongodb.scala.{
-  Document,
-  MongoClient,
-  MongoCollection,
-  MongoDatabase,
-  SingleObservableFuture,
-  result
-}
 
+import org.mongodb.scala._
+import org.mongodb.scala.model._
+import org.mongodb.scala.result._
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import persistence.DBDAO
-import scala.util.Try
+import scala.util.{Try, Success, Failure}
 import play.api.libs.json.Json
-import scala.util.Failure
-import scala.util.Success
 import org.bson.json.JsonObject
-import org.mongodb.scala.model.Filters
-import org.mongodb.scala.model.UpdateOptions
-import com.mongodb.client.model.ReplaceOptions
+import org.mongodb.scala.bson.collection.immutable.Document
 
-class MongoDBDAO extends DBDAO:
+class MongoDBDAO extends DBDAO {
+
   private val databaseDB: String = sys.env.getOrElse("MONGO_DB", "tbl")
-  private val databaseUser: String =
-    sys.env.getOrElse("MONGO_USERNAME", "root")
-  private val databasePassword: String =
-    sys.env.getOrElse("MONGO_PASSWORD", "mongo")
+  private val databaseUser: String = sys.env.getOrElse("MONGO_USERNAME", "root")
+  private val databasePassword: String = sys.env.getOrElse("MONGO_PASSWORD", "mongo")
   private val databasePort: String = sys.env.getOrElse("MONGO_PORT", "27017")
-  private val databaseHost: String =
-    sys.env.getOrElse("MONGO_HOST", "mongo")
+  private val databaseHost: String = sys.env.getOrElse("MONGO_HOST", "mongo")
+  
+  private val databaseURI: String = 
+    s"mongodb://$databaseUser:$databasePassword@$databaseHost:$databasePort/?authSource=admin&maxPoolSize=50&minPoolSize=5&maxIdleTimeMS=30000&connectTimeoutMS=10000"
 
-  private val databaseURI: String =
-    s"mongodb://$databaseUser:$databasePassword@$databaseHost:$databasePort/?authSource=admin"
   private val client: MongoClient = MongoClient(databaseURI)
   private val db: MongoDatabase = client.getDatabase(databaseDB)
-  private val gameCollection: MongoCollection[JsonObject] =
-    db.getCollection("game")
+  private val gameCollection: MongoCollection[JsonObject] = db.getCollection("game")
 
   override def delete(): Future[Unit] = {
-    Try {
-      Await.result(gameCollection.drop().head, 10.seconds)
-    } match
-      case Failure(exception) =>
-        println(exception)
-        Future.failed(exception)
-      case Success(value) =>
-        Future.successful(())
+    gameCollection.drop().toFuture().map(_ => ())
   }
+
   override def create(): Future[Unit] = {
-    Try {
-      Await.result(db.createCollection("game").head, 10.seconds)
-      Await.result(db.listCollectionNames().head, 10.seconds)
-    } match
-      case Failure(exception) =>
-        println(exception)
-        Future.failed(exception)
-      case Success(value) =>
-        Future.successful(())
+    for {
+      _ <- db.createCollection("game").toFuture()
+      _ <- gameCollection.createIndex(Indexes.ascending("_id")).toFuture()
+    } yield ()
   }
+
   override def save(game: String): Future[Int] = {
-    Try(
-      Await.result(
-        gameCollection
-          .replaceOne(
-            Filters.eq("_id", 1),
-            new JsonObject(game),
-            ReplaceOptions().upsert(true)
-          )
-          .head(),
-        30.seconds
+    gameCollection
+      .replaceOne(
+        Filters.eq("_id", 1),
+        new JsonObject(game),
+        ReplaceOptions().upsert(true)
       )
-    ) match {
-      case Failure(exception) =>
-        println(s"$exception, $game")
-        Future.failed(new IllegalArgumentException(exception))
-      case Success(value) =>
-        Future.successful(1)
-    }
+      .toFuture()
+      .map(_ => 1)
+      .recover {
+        case exception =>
+          println(s"$exception, $game")
+          throw new IllegalArgumentException(exception)
+      }
   }
+
   override def load(): Future[Option[String]] = {
-    Try(
-      Await.result(
-        gameCollection.find().first().head(),
-        30.seconds
-      )
-    ) match {
-      case Failure(exception) =>
-        println(s"$exception")
-        Future.failed(exception)
-      case Success(value) =>
-        Future.successful(Some(value.getJson()))
-    }
+    gameCollection
+      .find()
+      .first()
+      .toFuture()
+      .map(doc => Some(doc.getJson()))
+      .recover {
+        case exception =>
+          println(s"$exception")
+          None
+      }
   }
+
   override def closeDatabase(): Unit = {
     client.close()
   }
+}
