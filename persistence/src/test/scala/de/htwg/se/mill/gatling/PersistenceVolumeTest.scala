827ef582405c57fa606e11f70af49c5ece27dfed
Melanie Galip <me391gal@htwg-konstanz.de>
2024-06-11 20:33:45 +0200

    feat: test Stream in gatlingtest

--- persistence/src/test/scala/de/htwg/se/mill/gatling/PersistenceVolumeTest.scala
+++ persistence/src/test/scala/de/htwg/se/mill/gatling/PersistenceVolumeTest.scala
@@ -12,15 +12,81 @@ import io.gatling.core.structure.ChainBuilder
 import io.gatling.core.structure.PopulationBuilder
 import io.gatling.core.body.Body
 import akka.http.javadsl.model.HttpMethod
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Sink, Source, Flow}
+import akka.stream.{ActorMaterializer, Materializer}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json._
+import scala.concurrent.Await
+import play.api.libs.json.Writes.mapWrites
+
+object TestDataStream {
+  implicit val system: ActorSystem = ActorSystem("TestDataStreamSystem")
+  implicit val materializer: Materializer = ActorMaterializer()
+
+  var game =
+    Json.parse(
+      scala.io.Source
+        .fromURL(getClass.getResource("/game_volume.json"))
+        .mkString
+    )
+
+  val testDataSource: Source[String, _] =
+    Source.fromIterator(() => Iterator.continually(generateTestData))
+
+  def generateTestData: String = {
+    val field = Map.apply(
+      "x" -> scala.util.Random.nextInt(3).toString(),
+      "y" -> scala.util.Random.nextInt(3).toString(),
+      "ring" -> scala.util.Random.nextInt(3).toString(),
+      "color" -> (scala.util.Random.nextInt(3) match
+        case 0 => "⚫"
+        case 1 => "🔴"
+        case 2 => "🔵"
+      )
+    )
+    val fields = (game \ "gameState" \ "game" \ "board" \ "fields").get
+      .as[List[Map[String, String]]]
+
+    def equalsField(f: Map[String, String]) = f.get("x") == field.get("x")
+      && f.get("y") == field.get("y")
+      && f.get("ring") == field.get("ring")
+
+    game = game
+      .transform(
+        (__ \ "gameState" \ "game" \ "board" \ "fields").json.update(
+          __.read[List[Map[String, String]]]
+            .map(_ =>
+              Json.toJson(fields.map {
+                case f if equalsField(f) => field; case x => x
+              })
+            )
+        )
+      )
+      .asOpt
+      .get
+
+    game.toString
+  }
+
+  def getTestData(count: Int): Future[String] = {
+    testDataSource.take(count).runWith(Sink.head)
+  }
+}
 
 class PersistenceVolumeTest extends SimulationSkeleton {
+
+  val testDataCount = 100
+  val testDataFuture = TestDataStream.getTestData(testDataCount)
+  val testData = Await.result(testDataFuture, 10.seconds)
 
   override val operations = List(
     buildOperation(
       "persistence save",
       "POST",
       "/persistence/save",
-      ElFileBody("game_volume.json")
+      StringBody(testData)
     ),
     buildOperation(
       "persistence load",
@@ -57,4 +123,12 @@ class PersistenceVolumeTest extends SimulationSkeleton {
   }
 
   executeOperations()
+}
+
+object Test {
+  @main def main: Unit = {
+    val testDataCount = 100
+    val testDataFuture = TestDataStream.getTestData(testDataCount)
+    val testData = Await.result(testDataFuture, 10.seconds)
+  }
 }
--- persistence/src/test/scala/de/htwg/se/mill/gatling/SimulationSkeleton.scala
+++ persistence/src/test/scala/de/htwg/se/mill/gatling/SimulationSkeleton.scala
@@ -13,7 +13,6 @@ import io.gatling.core.structure.PopulationBuilder
 import io.gatling.core.body.Body
 import akka.http.javadsl.model.HttpMethod
 
-
 abstract class SimulationSkeleton extends Simulation {
 
   val operations: List[ChainBuilder]
@@ -21,11 +20,15 @@ abstract class SimulationSkeleton extends Simulation {
   val httpProtocol = http
     .baseUrl("http://localhost:8081")
     .inferHtmlResources()
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+    .acceptHeader(
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+    )
     .acceptEncodingHeader("gzip, deflate, br")
     .acceptLanguageHeader("de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7")
     .upgradeInsecureRequestsHeader("1")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+    .userAgentHeader(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    )
 
   val headers = Map(
     "Cache-Control" -> "max-age=0",
@@ -38,7 +41,12 @@ abstract class SimulationSkeleton extends Simulation {
     "sec-ch-ua-platform" -> "Windows"
   )
 
-  def buildOperation(name: String, request: String, operation: String, body: Body): ChainBuilder = {
+  def buildOperation(
+      name: String,
+      request: String,
+      operation: String,
+      body: Body
+  ): ChainBuilder = {
     exec(
       http(name)
         .httpRequest(request, operation)
@@ -49,7 +57,7 @@ abstract class SimulationSkeleton extends Simulation {
   def buildScenario(name: String) =
     scenario(name)
       .exec(
-        //exec the operations and between each pause a second
+        // exec the operations and between each pause a second
         operations.reduce((a, b) => a.pause(1.second).exec(b))
       )
 
